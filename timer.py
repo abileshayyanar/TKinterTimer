ca8cf024bf50af9ec54929b4e0da3ccb6f2510b5
Abilesh Ayyanar <abileshayyanar01@gmail.com>
2025-10-17 13:47:52 -0600

    Declared necessary functions (need to add functionality)

--- timer.py
+++ timer.py
@@ -3,11 +3,24 @@ import tkinter as tk
 window = tk.Tk()
 window.title("Timer")
 
+def startStop():
+
+def updateTimer():
+
+
+def updateTime():
+    window.after(1000, updateTimer)
+
+
+
 # Create frame to hold widgets
-frame = tk.Frame(master=window, width=350, height=150)
+frame = tk.Frame(master=window, width=350, height=175)
 frame.pack()
 
 # Create and place labels and entry boxes
+
+time = tk.Label(master=frame, text="00:00:00", font=('arial', 50))
+time.place(x=40, y=110)
 label = tk.Label(master=frame, text="Set timer:")
 hourLabel = tk.Label(master=frame, text = "Hours")
 hourEntry = tk.Entry(master=frame, width = 4)
@@ -30,7 +43,14 @@ secEntry.place(x=240, y=35)
 secLabel.place(x=190, y=35)
 
 #Create button to start and stop timer
-pauseButton = tk.Button(master=frame, text="Start/Pause", width = 10)
+pauseButton = tk.Button(master=frame, text="Start/Pause", width = 10, command=startStop)
 pauseButton.place(x=120, y=80)
+
+
+
+def handle_click(event):
+    print("button clicked")
+pauseButton.bind("<Button-1>", handle_click)
+
 
 window.mainloop()
