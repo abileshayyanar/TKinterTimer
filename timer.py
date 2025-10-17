d97327b958305873fb95364b4f9910a994c0cff6
Abilesh Ayyanar <abileshayyanar01@gmail.com>
2025-10-17 11:28:22 -0600

    created initial UI

---
+++ timer.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Timer")
+
+frame = tk.Frame(master=window, width=350, height=150)
+frame.pack()
+
+label = tk.Label(master=frame, text="Set timer:")
+hourLabel = tk.Label(master=frame, text = "Hours")
+hourEntry = tk.Entry(master=frame, width = 4)
+
+minLabel = tk.Label(master=frame, text = "Minutes")
+minEntry = tk.Entry(master=frame, width = 4)
+
+secLabel = tk.Label(master=frame, text = "Seconds")
+secEntry = tk.Entry(master=frame, width = 4)
+
+label.place(x=5, y=0)
+
+hourEntry.place(x=60, y=35)
+hourLabel.place(x=20, y=35)
+
+minEntry.place(x=150, y=35)
+minLabel.place(x=100, y=35)
+
+secEntry.place(x=240, y=35)
+secLabel.place(x=190, y=35)
+
+
+window.mainloop()
